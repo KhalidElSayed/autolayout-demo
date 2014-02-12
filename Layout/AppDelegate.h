60962b15cf303fcb8d7f4f887f627ab54a48867b
Shagun Madhikarmi <shagun@ustwo.co.uk>
2014-02-12 21:35:27 +0000

    Initial Commit

---
+++ Layout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Layout
+//
+//  Created by Shagun Madhikarmi on 12/02/2014.
+//  Copyright (c) 2014 ustwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
